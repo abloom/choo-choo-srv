eef555aa678a8392b07bdb54cae1a05e13af58da
Andrew Bloom <andrew@andrewkbloom.com>
2023-09-21 22:16:04 -0500

    make the prod.sh script smarter

---
+++ bin/prod.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+ROOT_DIR=$SCRIPT_DIR/..
+VENV_DIR=$ROOT_DIR/.venv
+VENV_BIN=$VENV_DIR/bin
+
+echo $VENV_BIN
+$VENV_BIN/pip install -r $ROOT_DIR/requirements.txt
+$VENV_BIN/waitress-serve --host 127.0.0.1 app:app
