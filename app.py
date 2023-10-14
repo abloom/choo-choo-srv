ae56e6bcca7074350490ddfac5ef23e276b4d6d0
Andrew Bloom <andrew@andrewkbloom.com>
2023-10-13 20:33:06 -0500

    basic times without trip updates

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-import datetime
+from collections import defaultdict
+from datetime import datetime, time
+from functools import cached_property
 import json
 import os
 from dateutil.parser import isoparse
@@ -9,6 +11,14 @@ from cache import cache
 from metra import MetraClient
 
 load_dotenv()
+
+ROUTES_AND_STOPS_TO_TRACK = {
+    # "UP-NW": [],
+    "UP-NW": ["JEFFERSONP"],
+    "MD-N": ["FORESTGLEN"],
+    # "UP-W": ["OTC"],
+    # "BNSF": [],
+}
 
 config = {
     "CACHE_TYPE": "SimpleCache",
@@ -23,7 +33,17 @@ cache.init_app(app)
 
 @app.template_filter()
 def to_json(obj: dict) -> str:
-    return json.dumps(obj, sort_keys = True, indent = 4, separators = (',', ': '))
+    return json.dumps(obj, sort_keys=True, indent=4, separators=(',', ': '), default=lambda o: o.__dict__)
+
+@app.template_filter()
+def from_iso_to_time(iso_time: str) -> datetime:
+    try:
+        return time.fromisoformat(iso_time)
+    except ValueError:
+        # some trips that end up running past midnight have an arrival time larger than 23 hours
+        time_parts = iso_time.split(":")
+        time_parts[0] = str(int(time_parts[0]) - 24).zfill(2)
+        return time.fromisoformat(":".join(time_parts))
 
 @app.template_filter()
 def from_iso_to_datetime(iso_date: str) -> datetime:
@@ -31,28 +51,90 @@ def from_iso_to_datetime(iso_date: str) -> datetime:
 
 @app.template_filter()
 def to_local_time(time: datetime) -> str:
-    return time.astimezone().strftime("%-I:%M %p")
+    return time.strftime("%-I:%M %p")
 
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template("500.html"), 500
 
+class MetraStop:
+    def __init__(self, route, stop) -> None:
+        self.stop_sequence = None
+        self.route = route
+        self.stop = stop
+        self._times = {
+            0: [],
+            1: [],
+        }
+
+    @cached_property
+    def times(self) -> dict:
+        filtered_times = {
+            0: [],
+            1: [],
+        }
+
+        now = datetime.now().time()
+        for direction, stop_times in self._times.items():
+            for stop_time in stop_times:
+                arrival_time = from_iso_to_time(stop_time["arrival_time"])
+                if now <= arrival_time:
+                    filtered_times[direction].append(stop_time)
+
+        return filtered_times
+
+
+    def add_time(self, direction_id, stop_time) -> None:
+        if stop_time["stop_id"] == self.stop["stop_id"]:
+            if direction_id == 0:
+                self.stop_sequence = stop_time["stop_sequence"]
+            self._times[direction_id].append(stop_time)
+
+    def sort_times(self) -> None:
+        for direction_id, stop_times in self.times.items():
+            self._times[direction_id] = sorted(stop_times, key=lambda x: x["arrival_time"])
+
 @app.route("/metra")
 def metra():
-    client = MetraClient(app.config["METRA_USER"], app.config["METRA_PASS"])
-    stops = client.stops()
-    routes = client.routes()
-    trips = client.trips()
+    client = MetraClient(app.config["METRA_USER"], app.config["METRA_PASS"], ROUTES_AND_STOPS_TO_TRACK)
 
+    routes = client.routes()
+    calendars = client.calendars()
+    service_ids = [c["service_id"] for c in calendars]
+
+    trips = client.trips(service_ids)
     trip_ids = trips.keys()
-    trip_updates = client.trip_updates(trip_ids)
-    trip_updates.reverse()
+
+    # trip_updates = client.trip_updates(trip_ids)
+    stop_times_by_stop_id = client.stop_times(trip_ids)
+    stops = client.stops()
+
+    metra_stops_by_route_and_stop = defaultdict(lambda: {})
+    for stop_id, stop_times in stop_times_by_stop_id.items():
+        stop = stops[stop_id]
+
+        for stop_time in stop_times:
+            trip = trips[stop_time["trip_id"]]
+            route = routes[trip["route_id"]]
+
+            route_id = route["route_id"]
+            if not ROUTES_AND_STOPS_TO_TRACK or route_id in ROUTES_AND_STOPS_TO_TRACK:
+                stops_to_track = ROUTES_AND_STOPS_TO_TRACK[route_id]
+                if not stops_to_track or stop_id in stops_to_track:
+                    if stop_id not in metra_stops_by_route_and_stop[route_id]:
+                        metra_stops_by_route_and_stop[route_id][stop_id] = MetraStop(route, stop)
+
+                    metra_stops_by_route_and_stop[route_id][stop_id].add_time(trip["direction_id"], stop_time)
+
+    metra_stops_by_route = {}
+    for route_id, stops_by_stop_id in metra_stops_by_route_and_stop.items():
+        metra_stops_by_route[route_id] = sorted(stops_by_stop_id.values(), key=lambda x: x.stop_sequence)
+        for stop in metra_stops_by_route[route_id]:
+            stop.sort_times()
 
     return render_template("metra.html",
-        trip_updates=trip_updates,
-        stops=stops,
         routes=routes,
-        trips=trips,
+        metra_stops_by_route=metra_stops_by_route,
         directions={
             0: "Outbound",
             1: "Inbound",
--- metra.py
+++ metra.py
@@ -1,20 +1,16 @@
+from datetime import date, datetime
+from pprint import pprint
 from cache import cache
 import requests
-
-ROUTES_AND_STOPS_TO_TRACK = {
-    "UP-NW": ["JEFFERSONP"],
-    # "UP-W": ["OTC"],
-    # "BNSF": [],
-}
-ROUTE_IDS = ROUTES_AND_STOPS_TO_TRACK.keys()
-
 
 class MetraClient:
     base_headers = {'Accept': 'application/json'}
 
-    def __init__(self, username: str, password: str) -> None:
+    def __init__(self, username: str, password: str, stops_by_route: dict[str, list[str]]) -> None:
         self.username = username
         self.password = password
+        self.stops_by_route = stops_by_route
+        self.route_ids = list(stops_by_route.keys())
 
     @property
     def base_url(self) -> str:
@@ -36,10 +32,11 @@ class MetraClient:
             if trip["trip_id"] in trip_ids:
                 route_id = trip["route_id"]
                 stop_time_updates = []
-                stop_ids_for_route = ROUTES_AND_STOPS_TO_TRACK.get(route_id)
+                stop_ids_for_route = self.stops_by_route.get(route_id)
                 for stop_time in trip_update["stop_time_update"]:
                     if not stop_ids_for_route or stop_time["stop_id"] in stop_ids_for_route:
                         stop_time_updates.append(stop_time)
+
                 trip_update["stop_time_update"] = stop_time_updates
 
                 if trip_update["stop_time_update"]:
@@ -47,12 +44,27 @@ class MetraClient:
         
         return trip_updates
 
-    @cache.cached(timeout=300, key_prefix="metra_trips")
-    def trips(self) -> dict:
+    @cache.cached(timeout=1440, key_prefix="metra_calendars")
+    def calendars(self) -> dict:
+        calendars = []
+        today = datetime.now()
+        day_of_week = today.strftime("%A").lower()
+        for calendar in self._get("/schedule/calendar"):
+            start_date = datetime.strptime(calendar["start_date"], "%Y-%m-%d")
+            end_date = datetime.strptime(calendar["end_date"], "%Y-%m-%d")
+            if start_date <= today <= end_date:
+                if calendar[day_of_week]:
+                    calendars.append(calendar)
+
+        return calendars
+
+    @cache.memoize(timeout=300)
+    def trips(self, service_ids) -> dict:
         trips = {}
         for trip in self._get("/schedule/trips"):
-            if not ROUTE_IDS or (trip["route_id"] in ROUTE_IDS):
-                trips[trip["trip_id"]] = trip
+            if not self.route_ids or (trip["route_id"] in self.route_ids):
+                if trip["service_id"] in service_ids:
+                    trips[trip["trip_id"]] = trip
 
         return trips
 
@@ -68,7 +80,20 @@ class MetraClient:
     def routes(self) -> dict:
         routes = {}
         for route in self._get("/schedule/routes"):
-            if not ROUTE_IDS or route["route_id"] in ROUTE_IDS:
+            if not self.route_ids or route["route_id"] in self.route_ids:
                 routes[route["route_id"]] = route
 
         return routes
+
+    @cache.memoize(timeout=300)
+    def stop_times(self, trip_ids) -> dict:
+        stop_times = {}
+        for stop_time in self._get("/schedule/stop_times"):
+            stop_id = stop_time["stop_id"]
+            if stop_time["trip_id"] in trip_ids:
+                if stop_id not in stop_times:
+                    stop_times[stop_id] = []
+
+                stop_times[stop_id].append(stop_time)
+
+        return stop_times
